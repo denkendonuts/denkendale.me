ec302192fdd8670b30a6e669db82a2f4199050a9
Dale Oconnell <daleoc@uvic.ca>
2021-02-23 13:40:57 -0800

    first commit

---
+++ scroll.js
@@ -0,0 +1,28 @@
+$(document).on("scroll", function () {
+  
+  var pixels = $(document).scrollTop();
+  
+
+    
+  if (pixels < 50) {
+    $("header").removeClass("hide")
+  } else {
+    $("header").addClass("hide")
+  }
+  
+  if (pixels < 600) {
+    $("body").css("background-color", "#ffffff")
+  } else if (pixels < 1400) {
+    $("body").css("background-color", "#a29c97")
+  } else if (pixels < 2200) {
+    $("body").css("background-color", "#d9dfe4")
+  } else if (pixels < 3000) {
+    $("body").css("background-color", "#fff0f0")
+  } else {
+    $("body").css("background-color", "#cdccc7")
+  }
+  
+  
+  
+
+})
